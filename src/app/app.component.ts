290482e8030e89b6bd5e9d6ece70398dc4be4650
Muzamil Ashfaq <muzamil.ashfaq@techno-soft.com>
2020-01-17 10:41:52 +0500

    sec

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,29 +14,36 @@ export class AppComponent implements OnInit {
 
   constructor(private authService: AuthService, private helperService: HelperService, private router: Router) {
 
-    // SMART.init({
+    SMART.init({
+      clientId: '6770df86-4872-4bf4-ae77-b71f92438634',
+      scope: `patient/Account.read patient/AllergyIntolerance.read patient/Appointment.read patient/Patient.read
+              patient/AllergyIntolerance.write patient/Appointment.write patient/Patient.write`
+    }).then(client => {
+      return Promise.all([
+        client.patient.read(),
+      ]);
 
-    // }).then(client => {
-
-    // });
+    }).then((values) => {
+      this.router.navigate(['patients']);
+    });
   }
 
 
 
   ngOnInit() {
-    if (!this.authService.hasAuthToken()) {
-      if (this.helperService.urlParam('code')) {
-        SMART.ready().then((smart) => {
-          const user = smart.patient;
-        }).catch(error => {
-          console.log(error);
-        });
-      } else {
-        console.log('Not Autherized');
-      }
-    } else {
-      this.router.navigate(['patients']);
-    }
+    // if (!this.authService.hasAuthToken()) {
+    //   if (this.helperService.urlParam('code')) {
+    //     SMART.ready().then((smart) => {
+    //       const user = smart.patient;
+    //     }).catch(error => {
+    //       console.log(error);
+    //     });
+    //   } else {
+    //     console.log('Not Autherized');
+    //   }
+    // } else {
+    //   this.router.navigate(['patients']);
+    // }
 
   }
 
